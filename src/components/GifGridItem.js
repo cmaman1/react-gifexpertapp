df8683d74fcd900a0b1e2733bc37d222a178b137
Carolina Mamani <mamaniccarolina@gmail.com>
2021-04-28 13:05:20 -0300

    First commit

---
+++ src/components/GifGridItem.js
@@ -0,0 +1,8 @@
+export const GifGridItem = ({ url, title }) => {
+  return (
+    <div className='card animate__animated animate__fadeIn'>
+      <img src={url} alt={title} />
+      <p>{title}</p>
+    </div>
+  );
+};
